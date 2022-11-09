02724f4cb636bd1951c549ba46ba3983e5878e88
RamS24 <AlSaadi.Rami24@gmail.com>
2022-11-09 11:52:54 +0100

    solution

--- task-04/src/Inputs.jsx
+++ task-04/src/Inputs.jsx
@@ -6,10 +6,14 @@ export default function Inputs(){
 const {state,dispatch} = useContext(Context);
   return (
     <div>
-    <label htmlFor="name">Update Name</label>
-    <input name='name' type="text" value={state.name} onChange={(e)=>{dispatch({type: e.target.name, payload:e.target.value})}} />
-    <label htmlFor="location">Update Location</label>
-    <input name='location' type="text"  value={state.location} onChange={(e)=>{dispatch({type: e.target.name, payload:e.target.value})}} />
+    <div className="input-item">
+        <label htmlFor="name">Update Name: </label>
+        <input name='name' type="text" value={state.name} onChange={(e)=>{dispatch({type: e.target.name, payload:e.target.value})}} />
+    </div>
+    <div className="input-item">
+        <label htmlFor="location">Update Location: </label>
+        <input name='location' type="text"  value={state.location} onChange={(e)=>{dispatch({type: e.target.name, payload:e.target.value})}} />
+    </div>
     </div>
   )
 }
