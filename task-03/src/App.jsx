08eac558822dfd4a7f462e1770fefd9fb45f3e6a
RamS24 <AlSaadi.Rami24@gmail.com>
2023-06-04 14:36:44 +0200

    task3 extra

--- task-03/src/App.jsx
+++ task-03/src/App.jsx
@@ -14,7 +14,23 @@ function App() {
   return <div className="App">
 <h1>Game of Thrones books </h1>
 {
-  data?.map((el)=>(Array.from(Object.entries(el))).map((detail,idx)=> <Detail key={idx} title={detail[0]} details={detail[1]} />))
+  data?.map((el)=>(Array.from(Object.entries(el))).map((detail,idx)=> {
+    switch(detail[0]){
+      case 'url':
+        detail[1] = <a href={detail[1]}>{detail[1]}</a>;
+        break;
+      case 'released':
+        detail[1] = new Date(detail[1]).toDateString();
+        break;
+      case 'characters':
+      case 'povCharacters':
+        detail[1] = detail[1].length;
+        break;
+        default:
+          break;
+    }
+    return <Detail key={idx} title={detail[0]} details={detail[1]} />
+    }))
 }
   </div>;
 }
